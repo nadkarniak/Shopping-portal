b5693b409052aceaa532144b592caf1264205f48
AkshatSavaliya <akshat.savaliya@gmail.com>
2020-03-28 15:54:24 -0400

    Initial commit

---
+++ src/main/java/edu/northeastern/cs5200/models/User.java
@@ -0,0 +1,12 @@
+package edu.northeastern.cs5200.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+  @Id
+  private Integer id;
+
+}
---
+++ src/test/java/edu/northeastern/cs5200/Cs5200Spring2020SavaliyaJpaApplicationTests.java
@@ -0,0 +1,20 @@
+package edu.northeastern.cs5200;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Cs5200Spring2020SavaliyaJpaApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+}
